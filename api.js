f63120cc9442a3879f0e0503634b28599a4b4abe
ninjason62 <jgreaves62@gmail.com>
2019-01-25 17:28:04 -0500

    test commit again

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ app.get('/players', service.getPlayers);
 app.get('/games', service.getGames);
 app.get('/results', service.getAllResults);
 
-//testing something
+//listen on port ending in 443 because of https
 app.listen(port, () =>{
     console.log("Listening on port " + port);
 })
