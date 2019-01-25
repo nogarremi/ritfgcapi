9df6dc677f3e3198c530129ad0e489b02141e6c2
ninjason62 <jxg4000@rit.edu>
2019-01-25 17:26:20 -0500

    Testing something real quick

--- api.js
+++ api.js
@@ -9,6 +9,7 @@ app.get('/players', service.getPlayers);
 app.get('/games', service.getGames);
 app.get('/results', service.getAllResults);
 
+//testing something
 app.listen(port, () =>{
     console.log("Listening on port " + port);
 })
