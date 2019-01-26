3d87aed60b3fd23fed1c856a4c4d2b7831069811
Jason Greaves <jgreaves62@gmail.com>
2019-01-25 20:18:19 -0500

    turned of seq logging

--- models/seq.js
+++ models/seq.js
@@ -5,7 +5,7 @@ var seq = new Sequelize('2185_ranbats', config.db_user, config.db_pass, {
     host: 'ritfgc.cwgxxuhgsu0i.us-east-1.rds.amazonaws.com',
     port: 3306,
     dialect: 'mysql',
-    logging: true,
+    logging: false,
     pool: {
         max: 500,
         min: 0,
