9a1835f530fa3783d7fb8b56c76a84d2e790ea57
ninjason62 <jgreaves62@gmail.com>
2019-01-30 18:18:17 -0500

    cert stuff: needed

--- api.js
+++ api.js
@@ -7,13 +7,13 @@ var service = require('./services/APIService');
 
 
 const app = express()
-const port = 8080
+const port = 443
 
-//var private_key = fs.readFileSync('/home/ec2-user/api/ia.key', 'utf8');
-//var cert = fs.readFileSync('/home/ec2-user/api/ia.crt', 'utf8');
-//var ca = fs.readFileSync('/home/ec2-user/api/ca.crt', 'utf8');
-//var creds = {key: private_key, cert: cert, ca: ca};
-//var http_server = https.createServer(creds, app);
+var private_key = fs.readFileSync('/home/ec2-user/ia.key', 'utf8');
+var cert = fs.readFileSync('/home/ec2-user/ia.crt', 'utf8');
+var ca = fs.readFileSync('/home/ec2-user/ca.crt', 'utf8');
+var creds = {key: private_key, cert: cert, ca: ca};
+var http_server = https.createServer(creds, app);
 
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
@@ -38,6 +38,6 @@ app.get('/results/:game_ID', service.GetGameTopThreeResults);
 app.post('/email', service.sendEmail);
 
 //listen on port ending in 443 because of https
-app.listen(port, () =>{
+http_server.listen(port, () =>{
     console.log("Listening on port " + port);
 })
