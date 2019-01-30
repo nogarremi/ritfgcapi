837d10893ad0f17a2a2fff55979fea992de45253
Jason Greaves <jgreaves@chessmh.com>
2019-01-30 12:54:03 -0500

    certs

--- api.js
+++ api.js
@@ -8,6 +8,11 @@ var service = require('./services/APIService');
 const app = express()
 const port = 8080
 
+var private_key = fs.readFileSync('/home/ec2-user/api/ia.key', 'utf8');
+var cert = fs.readFileSync('/home/ec2-user/api/ia.cert', 'utf8');
+var ca = fs.readFileSync('/home/ec2-user/api/ca.crt', 'utf8');
+var creds = {key: private_key, cert: cert, ca: ca};
+var http_server = https.createServer(creds, app);
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
@@ -31,6 +36,6 @@ app.get('/results/:game_ID', service.GetGameTopThreeResults);
 app.post('/email', service.sendEmail);
 
 //listen on port ending in 443 because of https
-app.listen(port, () =>{
+http_server.listen(port, () =>{
     console.log("Listening on port " + port);
 })
